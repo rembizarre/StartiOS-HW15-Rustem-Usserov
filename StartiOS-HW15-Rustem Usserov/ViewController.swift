e0f53ca21bcbf2adedf5a4bc8ab093cdd34fc25d
Remi <usrustem@gmail.com>
2024-05-10 00:19:04 +0500

    getData func

--- StartiOS-HW15-Rustem Usserov/ViewController.swift
+++ StartiOS-HW15-Rustem Usserov/ViewController.swift
@@ -11,8 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData(urlRequest: "https://cataas.com/api/cats")
     }
+    func getData(urlRequest: String) {
+        guard let url = URL(string: urlRequest) else {
+            print("Invalid URL")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+
+            if let response = response as? HTTPURLResponse {
+                print("Response status code: \(response.statusCode)")
+
+                if response.statusCode == 200 {
+                    guard let data = data, let dataAsString = String(data: data, encoding: .utf8) else {
+                        print("Data is nil or cannot be decoded")
+                        return
+                    }
+                    print("Data received: \(dataAsString)")
+                } else {
+                    print("Server responded with status code: \(response.statusCode)")
+                }
+            } else {
+                print("Failed to get HTTP response")
+            }
+        }.resume()
+    }
+
 
 
 }
